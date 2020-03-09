98411febc85da4eab4ab9077d0a633c3ad0db0b1
ctrlouis <louis.bty57@gmail.com>
2020-03-09 03:33:46 +0100

    init repo

---
+++ app/public/js/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+import AimTrainer from './vue/AimTrainer.js';
+
+
+function start() {
+    AimTrainer.init();    
+}
+
+// start only
+// when page is load
+start();
---
+++ app/routes/indexRouter.js
@@ -0,0 +1,15 @@
+"use strict";
+
+
+import express from 'express';
+const router = express.Router();
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index.html.pug', {
+        title: 'Aimtrainer | @ctrlouis'
+    });
+});
+
+export default router;
